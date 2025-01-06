b41bac15158f85d9674173a5308fda70331d0f10
Yalanskyi Roman <keito.eternal@gmail.com>
2025-01-06 21:56:01 +0200

    initial preparation for step 3 training

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ paths defined here are used in many places
 """
 
 DATA_PATH = '/kaggle/input/sentinel-step2-data' # path for datasets
-EXPER_PATH = '/kaggle/working/logs' # path for saving checkpoints
+EXPER_PATH = '/kaggle/input/extract-step-results/' # path for saving checkpoints
 SYN_TMPDIR = './datasets/'  # path for dumping synthetic data
 DEBUG = False # true: will make synthetic data only uses draw_checkboard and ignore other classes
 # DEBUG = False
